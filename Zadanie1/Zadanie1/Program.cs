1a175c3af14f8015dbda1d3e8cd2eb1a6bb617cd
s27808 <s27808@pjwstk.edu.pl>
2025-03-02 16:54:40 +0100

    zadanie avarage

--- Zadanie1/Zadanie1/Program.cs
+++ Zadanie1/Zadanie1/Program.cs
@@ -5,3 +5,11 @@ Console.WriteLine("Zmiana testowa 1");
 var a = 5;
 Console.WriteLine(a + " test2");
 
+int[] array = { 1, 2, 3, 4, 5, 44 };
+
+double getAvarage(int[] a)
+{
+    return a.Average();
+}
+
+Console.WriteLine(getAvarage(array));
